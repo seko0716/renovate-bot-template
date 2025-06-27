c91ed84dd9f6c5760f5d9ff8ca2e662e6a93dde8
sergey34 <sergey3440@gmail.com>
2025-06-27 20:46:05 +0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    platform: 'gitlab',
+    endpoint: 'https://gitlab.mycompany.com/api/v4',
+    onboarding: true,
+    requireConfig: false,
+    logLevel: 'info',
+    // repositories: ['group/project'], // если хотите запускать только для определённых реп
+};
